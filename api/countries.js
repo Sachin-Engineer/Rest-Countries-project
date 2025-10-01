15c5f15dafe68587ff77fef4a460bc0a8c73db0a
Sachin-Engineer <sachin.engineer.22@gmail.com>
2025-10-01 18:56:28 +0530

    tenth commit

--- api/countries.js
+++ api/countries.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({ message: "Hello from countries.js" });
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    try {
+        const response = await axios.get('https://www.apicountries.com/countries');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch countries' });
+    }
 };
